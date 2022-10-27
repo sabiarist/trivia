e1406e0f556b6d789b3c387038259b50c5ea3d9e
Aristide Sabi <sabiarist@gmail.com>
2022-10-27 02:21:24 +0000

    Initial commit

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -4,14 +4,27 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
 
-from models import setup_db, Question, Category
+from backend.models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, '')
+    )
     setup_db(app)
+    CORS(app, resources={r"*/api/*": {"origins": "*"}})
+
+    # CORS Headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
 
     """
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
@@ -26,7 +39,6 @@ def create_app(test_config=None):
     Create an endpoint to handle GET requests
     for all available categories.
     """
-
 
     """
     @TODO:
@@ -100,3 +112,6 @@ def create_app(test_config=None):
 
     return app
 
+
+if __name__ == '__main__':
+    create_app().run()
